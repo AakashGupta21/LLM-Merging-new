aaafe3f5bd774daed1cf94fc1fc5748a91211ad3
AakashGupta21 <149186801+AakashGupta21@users.noreply.github.com>
2024-10-11 04:26:53 +0530

    Update __init__.py

--- llm_merging/merging/__init__.py
+++ llm_merging/merging/__init__.py
@@ -1,6 +1,6 @@
 from llm_merging.merging.LlamaAvg import LlamaAvg
 from llm_merging.merging.TinyLlamaAvg import TinyLlamaAvg
 from llm_merging.merging.FlanT5Avg import FlanT5Avg
-from llm_merging.merging.FlanT5Avg import FlanT5GeoMed
+from llm_merging.merging.FlanT5GeoMed import FlanT5GeoMed
 
 ## TODO Add more merge methods here
