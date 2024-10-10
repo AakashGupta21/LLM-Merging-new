9119b0282fbc9c34454ad1715f13426d70faa372
AakashGupta21 <149186801+AakashGupta21@users.noreply.github.com>
2024-10-11 04:24:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 """Install the llm_merging library"""
 
-
 from setuptools import setup
 
 setup(
     name="llm_merging",
-    version=1.0,
-    description="starter code for llm_merging",
+    version="1.0",
+    description="Starter code for llm_merging",
     install_requires=[
-        "torch", "ipdb"
+        "torch",
+        "ipdb",
+        "transformers",  # Required for model handling
+        "numpy",         # For numerical operations
+        "scipy",         # For advanced mathematical functions
+        "datasets",      # For dataset handling
+        "tqdm",          # For progress tracking
+        "pandas",        # For data manipulation and CSV handling
+        "safetensors"    # For saving merged models in the Safetensors format
     ],
     packages=["llm_merging"],
     entry_points={
         "llm_merging.merging.Merges": [
             "llama_avg = llm_merging.merging.LlamaAvg:LlamaAvg",
-            "flan_t5_avg = llm_merging.merging.FlanT5Avg:FlanT5Avg"
+            "flan_t5_avg = llm_merging.merging.FlanT5Avg:FlanT5Avg",
+            "flant5_geomed = llm_merging.merging.FlanT5GeoMed:FlanT5GeoMed"
         ]
     },
 )
